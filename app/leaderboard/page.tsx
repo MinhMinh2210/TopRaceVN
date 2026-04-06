fc94b8fa870a6cccb569b929e52a9182c27e8fa4
EVA-01 <Tranlinhjessica@gmail.com>
2026-04-06 17:21:28 +0700

    Final_TopRaceVN_Ver1.0.4_UpdateCategoty_Vehice

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -272,6 +272,7 @@ export default function LeaderboardPage() {
                 <SelectItem value="xe_ga">Xe ga</SelectItem>
                 <SelectItem value="xe_số">Xe số</SelectItem>
                 <SelectItem value="xe_côn_tay">Xe côn tay</SelectItem>
+                <SelectItem value="xe_dien">Xe điện</SelectItem>
                 <SelectItem value="oto">Ô tô</SelectItem>
               </SelectContent>
             </Select>
